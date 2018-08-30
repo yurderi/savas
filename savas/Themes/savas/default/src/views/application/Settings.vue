f8f8d9d5098e0fdd652e8a1e29e98c351aed9fd3
Tommy <tyurderi@yahoo.de>
2018-08-30 17:50:42 +0200

    Put channel/platforms grids to tabs

--- savas/Themes/savas/default/src/views/application/Settings.vue
+++ savas/Themes/savas/default/src/views/application/Settings.vue
@@ -4,43 +4,36 @@
         <v-content>
             <v-breadcrumb :items="breadcrumb"></v-breadcrumb>
             <v-form @submit.prevent="submit">
-                <div class="form-item">
-                    <label for="channels">
-                        Channels
-                    </label>
-                    <v-inline-grid :config="gridConfig.channels">
-                        <template slot="form" slot-scope="{ model }">
-                            <div class="form-item">
-                                <label for="label">Label</label>
-                                <v-input type="text" id="label" v-model="model.label"></v-input>
-                            </div>
-                            <div class="form-item">
-                                <label for="description">Description</label>
-                                <v-input type="textarea" id="description" v-model="model.description"></v-input>
-                            </div>
-                        </template>
-                    </v-inline-grid>
-                </div>
-                <div class="form-item">
-                    <label for="platforms">
-                        Platforms
-                    </label>
-                    <v-inline-grid :config="gridConfig.platforms">
-                        <template slot="form" slot-scope="{ model }">
-                            <div class="form-item">
-                                <label for="label">Label</label>
-                                <v-input type="text" id="label" v-model="model.label"></v-input>
-                            </div>
-                            <div class="form-item">
-                                <label for="description">Description</label>
-                                <v-input type="textarea" id="description" v-model="model.description"></v-input>
-                            </div>
-                        </template>
-                    </v-inline-grid>
-                </div>
-                <div class="form-buttons">
-                    <v-button>Submit</v-button>
-                </div>
+                <v-tab-menu>
+                    <v-tab id="channels" label="Channels">
+                        <v-inline-grid :config="gridConfig.channels">
+                            <template slot="form" slot-scope="{ model }">
+                                <div class="form-item">
+                                    <label for="label">Label</label>
+                                    <v-input type="text" id="label" v-model="model.label"></v-input>
+                                </div>
+                                <div class="form-item">
+                                    <label for="description">Description</label>
+                                    <v-input type="textarea" id="description" v-model="model.description"></v-input>
+                                </div>
+                            </template>
+                        </v-inline-grid>
+                    </v-tab>
+                    <v-tab id="platforms" label="Platforms">
+                        <v-inline-grid :config="gridConfig.platforms">
+                            <template slot="form" slot-scope="{ model }">
+                                <div class="form-item">
+                                    <label for="label">Label</label>
+                                    <v-input type="text" id="label" v-model="model.label"></v-input>
+                                </div>
+                                <div class="form-item">
+                                    <label for="description">Description</label>
+                                    <v-input type="textarea" id="description" v-model="model.description"></v-input>
+                                </div>
+                            </template>
+                        </v-inline-grid>
+                    </v-tab>
+                </v-tab-menu>
             </v-form>
         </v-content>
     </div>
