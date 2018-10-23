6102446cd5292089ef3319bde003409fac5f3b4d
Tommy <tyurderi@yahoo.de>
2018-10-23 17:47:42 +0200

    Fix migrations for new table structure

--- savas/Migrations/001_add_tables.php
+++ savas/Migrations/001_add_tables.php
@@ -13,21 +13,22 @@ class Migration_1 extends Migration
             CREATE TABLE s_platform (
               `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
               `userID` INT(11) NOT NULL,
-              `label` VARCHAR(255) NOT NULL,
-              `description` TEXT
+              `label` VARCHAR(255) NOT NULL
             );
             
             CREATE TABLE s_channel (
               `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
               `userID` INT(11) NOT NULL,
               `label` VARCHAR(255) NOT NULL,
-              `description` TEXT
+              `short` VARCHAR(3),
+              `main` TINYINT(1) DEFAULT 0
             );
             
             CREATE TABLE s_application (
               `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
               `label` VARCHAR(255) NOT NULL,
               `description` TEXT,
+              `visibility` VARCHAR(32),
               `privateKey` VARCHAR(32) NOT NULL,
               `publicKey` VARCHAR(32) NOT NULL,
               `created` DATETIME NOT NULL,
@@ -38,6 +39,7 @@ class Migration_1 extends Migration
               `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
               `appID` INT(11) NOT NULL,
               `channelID` INT(11) NOT NULL,
+              `active` TINYINT(4) DEFAULT 0,
               `version` VARCHAR(16) NOT NULL,
               `description` TEXT,
               `created` DATETIME NOT NULL,
@@ -55,7 +57,10 @@ class Migration_1 extends Migration
               `releaseID` INT(11) NOT NULL,
               `platformID` INT(11) NOT NULL,
               `filename` VARCHAR(255) NOT NULL,
+              `displayName` VARCHAR(255) NOT NULL,
               `size` INT(11) NOT NULL,
+              `extension` VARCHAR(255) NOT NULL,
+              `mimeType` VARCHAR(255) NOT NULL,
               `created` DATETIME NOT NULL,
               `changed` DATETIME NOT NULL
             );
